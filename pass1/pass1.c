03f1bc32c5a81c35cfc4735d2737e61a979ca35d
Kuruvilla Jacob <36655830+KuruvillaJacob02@users.noreply.github.com>
2023-11-02 16:10:16 +0530

    Update pass1.c

--- pass1/pass1.c
+++ pass1/pass1.c
@@ -2,61 +2,116 @@
 #include <string.h>
 #include <ctype.h> 
 #include <stdlib.h>
-int main() {
-    int start;
-    int locctr = 0;
-    char * ptr;
-    FILE *f1 = fopen("input.txt", "r");
-    FILE *f2 = fopen("intermediate.txt","w");
-    FILE *f3 = fopen("symtab.txt","w");
-    char line[100];char label[20];char symbol[20];char address[20];int count = 0;
-    while (fgets(line, sizeof(line), f1)) {
-    	count++;
-    	char *token = strtok(line, "\t");
-        // Check if the line starts with whitespace
-        if (isspace(line[0])) {
-		strcpy(symbol,token); //SYMBOL STORED        	
-		token = strtok(NULL, "\t");
-        	strcpy(address,token); // ADDRESS STORED
-        	fprintf(f2,"%x\t\t%s\t%s\n", locctr,symbol, address);
-        	locctr += 3;
-        }
-        else{
-        	strcpy(label,token); //LABEL STORED
-        	token = strtok(NULL, "\t");
-        	strcpy(symbol,token); //SYMBOL STORED
-        	token = strtok(NULL, "\t");
-        	strcpy(address,token); // ADDRESS STORED
-        	if (strcmp("START",symbol) == 0 && count ==1){
-        		start = strtol(address,NULL,16);
-        		locctr = start;
-        	}
-        	else if (count == 1){
-        		locctr = 0;
-        		fprintf(f2, "%x\t%s\t%s\t%s\n", locctr, label, symbol, address);
-        	}
-        	fprintf(f3, "%x\t%s\n",locctr,label);
-        	fprintf(f2, "%x\t%s\t%s\t%s\n", locctr, label, symbol, address);
-        	
-        	if (strcmp("WORD", symbol) == 0)
-        	 locctr += 3;
-            	else if (strcmp("RESB", symbol) == 0)
-                	locctr += strtol(address,NULL,16);
-            	else if (strcmp("RESW", symbol) == 0) 
-               	locctr += 3 * strtol(address,NULL,16);             	
-            	else if (strcmp("BYTE", symbol) == 0) 
-                	locctr += strlen(address)-4;
-            	else if (strcmp("START",symbol) == 0)
-            		locctr+=0;
-            	else if (strcmp("END",symbol) == 0)
-            		break;
-            	else
-			locctr+=3;   	
+int checkOp(char symbol[]) {
+    int flag = 0;
+    char comp[30];
+    FILE *f5 = fopen("optab.txt", "r");
+    while (fscanf(f5, "%s", comp) != EOF) {
+    	//printf("%s\n",comp);
+        fscanf(f5, "%*s");	
+        if (strcmp(symbol, comp) == 0) {
+            flag = 1;
+            break;  // No need to continue once a match is found
         }
     }
+    fclose(f5);
+    return flag;
+}
+int checkSym(char symbol[]) {
+    int flag = 0;
+    char comp[30];
+    FILE *f3 = fopen("symtab.txt", "r");
+    while (fscanf(f3, "%s", comp) != EOF) {
+    	//printf("%s\n",comp);
+        fscanf(f3, "%*s");	
+        if (strcmp(symbol, comp) == 0) {
+            flag = 1;
+            break;  // No need to continue once a match is found
+        }
+    }
+    fclose(f3);
+    return flag;
+}
+int main() {
+    FILE *f7 = fopen("symtab.txt","w");
+    fclose(f7);
+    int check;
+    int start;
+    int locctr = 0;
+    FILE *f1 = fopen("input.txt", "r");
+    FILE *f2 = fopen("intermediate.txt","w");
+    FILE *f5 = fopen("length.txt","w");
+    char line[100];char label[20];char symbol[20];char address[20];
+    fgets(line, sizeof(line), f1);  //READ FIRST LINE
+    char *token = strtok(line, "\t");
+    strcpy(label,token); //LABEL STORED
+    token = strtok(NULL, "\t");
+    strcpy(symbol,token); //SYMBOL STORED
+    token = strtok(NULL, "\t");
+    strcpy(address,token); // ADDRESS STORED
+    if (strcmp(symbol,"START") == 0){
+    	start = strtol(address,NULL,16);
+        locctr = start;
+        fprintf(f2, "%x\t%s\t%s\t%s", locctr, label, symbol, address);
+    }
+    else 
+    	locctr = 0;
+    // READ REMAINING LINES
+    while (fgets(line, sizeof(line), f1)) {
+    	char *token = strtok(line, "\t");
+        if (isspace(line[0])){
+		strcpy(label,"\0");
+        	strcpy(symbol,token); // SYMBOL STORED
+        	token = strtok(NULL, "\t");
+        	strcpy(address,token); // ADDRESS STORED
+        }
+	else{	
+		strcpy(label,token);
+		token = strtok(NULL, "\t");
+        	strcpy(symbol,token); // SYMBOL STORED
+        	token = strtok(NULL, "\t");
+        	strcpy(address,token); // ADDRESS STORED
+	}
+	fprintf(f2, "%x\t%s\t%s\t%s", locctr, label, symbol, address);
+	if (strlen(label) != 0){      //CHECK SYMBOL
+		if (checkSym(label) == 0){
+			FILE *f4 = fopen("symtab.txt", "a");
+			fprintf(f4, "%s\t%x\n",label,locctr);
+			fclose(f4);
+		}
+		else{
+			printf("\nDUPLICATE SYMBOL");
+			FILE *f7 = fopen("symtab.txt","w");
+			fclose(f7);
+			//remove("intermediate.txt");
+			//remove("length.txt");
+			break;
+		}
+	}
+	if (strcmp("WORD", symbol) == 0)	//CHECK OPCODE
+         	locctr += 3;
+        else if (strcmp("RESB", symbol) == 0)
+               locctr += strtol(address,NULL,16);
+        else if (strcmp("RESW", symbol) == 0) 
+               locctr += 3 * strtol(address,NULL,16);             	
+        else if (strcmp("BYTE", symbol) == 0) 
+                locctr += strlen(address)-4;
+        else if (strcmp("END",symbol) == 0)
+            	break;
+        else if (checkOp(symbol) == 1)
+		locctr+=3;
+	else{
+		printf("\nInvalid OPCODE");
+		FILE *f7 = fopen("symtab.txt","w");
+		fclose(f7);
+		//remove("intermediate.txt");
+		//remove("length.txt");
+	}
+		   	
+    }
+    locctr = locctr- start;
+    fprintf(f5,"%x",locctr);
     fclose(f1);
     fclose(f2);
-    fclose(f3);
     return 0;
 }
-
